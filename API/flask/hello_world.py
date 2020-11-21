04864f9e0422d2ed2d7b41396e3e99dbd64acaeb
Luketss <lucassilluzio@gmail.com>
2020-11-20 23:25:12 -0300

    hello com param pela url para nome na tela

--- API/flask/hello_world.py
+++ API/flask/hello_world.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s</h1>' % name
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
