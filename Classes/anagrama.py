0db7e53df1a449b55a416815109fe8cf12bddedd
Luketss <lucassilluzio@gmail.com>
2020-12-30 22:16:41 -0300

    brincando com palavras

---
+++ Classes/anagrama.py
@@ -0,0 +1,11 @@
+def anagramas(palavra):
+    if len(palavra) <= 1:
+        return palavra
+    else:
+        tmp = []
+        for value in anagramas(palavra[1:]):
+            for i in range(len(palavra)):
+                tmp.append(value[:i] + palavra[0:1] + value[i:])
+        return tmp
+
+print(anagramas('l'))
