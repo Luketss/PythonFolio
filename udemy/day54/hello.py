f2f869ea9e601adeb796712edca20492e6fdf85c
Luketss <lucassilluzio@gmail.com>
2021-08-04 12:23:56 -0300

    day 54

--- udemy/day54/hello.py
+++ udemy/day54/hello.py
@@ -2,9 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0] == True:
+            function()
+        return wrapper
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/bye')
+@make_bold
+def bye():
+    return "Bye!"
+
+@app.route('/username/<name>')
+def greet(name):
+    return f"Hello {name}"
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
