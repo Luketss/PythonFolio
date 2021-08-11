4b7eaaae1aac46290aa6aae85b9ff1dbe345be85
Luketss <lucassilluzio@gmail.com>
2021-08-11 14:47:29 -0300

    day 57

---
+++ udemy/day57/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    current_year = datetime.datetime.now().year
+    return render_template('index.html', year=current_year)
+
+if __name__ == "__main__":
+    app.run(debug=True)
